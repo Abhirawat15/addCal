257ca71f460a707fabca2882e93e7f2bfef46bac
Abhirawat15 <abhishekrawatvs158@gmail.com>
2023-08-27 13:00:45 +0530

    first origin

---
+++ front-end/index.js
@@ -0,0 +1,12 @@
+var result=document.querySelector('#result');
+var submit=document.querySelector('#submit');
+var ans=document.querySelector('#ans');
+
+submit.addEventListener('click',(e)=>{
+    var x=document.querySelector('#x');
+    x=parseInt(x.value);
+    var y=document.querySelector('#y');
+    y=parseInt(y.value);
+    result.value=x+y;
+    ans.innerHTML="Ans is: "+result.value;
+})
